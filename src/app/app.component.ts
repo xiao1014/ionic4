f23975752502e44fee516b7d4e3fbfc1352f32f6
what1014 <294883221@qq.com>
2018-11-05 08:33:25 +0800

    change status bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.scss']
 })
 export class AppComponent {
   constructor(
@@ -19,7 +20,9 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
+      this.statusBar.overlaysWebView(false);
+      this.statusBar.styleBlackTranslucent();
+      this.statusBar.backgroundColorByHexString('#f95551');
       this.splashScreen.hide();
     });
   }
